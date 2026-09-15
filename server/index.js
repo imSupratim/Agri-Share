feabc72d9f76e744c78e71f8f04ce1753114c453
Supratim Mandal <supratimm98@gmail.com>
2026-09-15 17:27:16 +0530

    cors frontend url

--- server/index.js
+++ server/index.js
@@ -16,7 +16,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://agri-share-blond.vercel.app",
     credentials: true,
   }),
 );
